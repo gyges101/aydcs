2ebf6cc0f88b85fa586a1dd8d017af028403a75d
gyges101 <61121942+gyges101@users.noreply.github.com>
2020-10-01 07:43:11 +0200

    Update gyges.js

--- gyges.js
+++ gyges.js
@@ -2,4 +2,5 @@ function gyges() {
 				$("#btnEnviar").removeAttr("disabled");
 				$("#btnSiguiente").removeAttr("disabled");
 				$( "#datepicker" ).datepicker( "option", "disabled", false );
+	                        document.getElementById("btnEnviar").click();
 			}
